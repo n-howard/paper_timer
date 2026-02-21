8e5ed11554f0605a58eb4d2edde95db7433980d8
Nat Howard <nhoward@oberlin.edu>
2026-02-21 22:37:37 +0000

    new flip version but only seconds and only sample

--- app/page.tsx
+++ app/page.tsx
@@ -3,40 +3,43 @@ import Image from "next/image";
 export default function Home() {
   return (
         <main>
-          <div className="bg-[url(/paperBackground.jpg)] flex min-h-screen items-center justify-center">
-            <div className="text-8xl text-stone-700 border-8 border-stone-500/10 grid grid-cols-3 gap-x-px rounded shadow-2xl font-[Patrick_Hand]">
-              <div className="relative bg-[url(/border.png)] p-8">
-                <div className="absolute inset-0 grid grid-rows-2">
-                  <div className="bg-linear-to-br from-stone-700/10 to-stone-900/10"></div>
-                  <div className="bg-linear-to-br from-stone-700/10 to-stone-900/10"></div>
+          <div className="bg-[url(/paperBackground.jpg)] min-h-screen">
+          <div className="pt-[100px] box-border min-h-none">
+            <div className="flex gap-[30px] font-[Patrick_Hand] text-8xl">
+              <div className="pl-[10px] flex gap-[10px] text-center">
+                {/* Segment 1 */}
+                <div className="block w-[100px] font-black">
+                  <div className="relative h-full">
+                    {/* Regular */}
+                    <div className="leading-[1.5] bg-stone-900 text-white w-full h-1/2 relative overflow-hidden text-center">2</div>
+                    <div className="leading-[0] bg-stone-800 text-white w-full h-1/2 relative overflow-hidden text-center">2</div>
+                  
+                  
+                  <div className="absolute h-full w-[100px] top-[0px] perspective-[400px]">
+                    {/* Overlay */}
+                    <div className="absolute overflow-hidden text-center w-full h-1/2 top-[0] leading-[1.5] bg-stone-900 text-white overlay-bottom">2</div>
+                    <div className="border-t-2 border-black absolute bottom-[0] leading-[0] overflow-hidden text-center w-full h-1/2 bg-stone-800 text-white overlay-top">2</div>
+                  </div>
+                  </div>
+                  </div>
+
+                {/* Segment 2 */}
+                <div className="block w-[100px] font-black">
+                  <div className="relative h-full">
+                    {/* Regular */}
+                    <div className="leading-[1.5] bg-stone-900 text-white w-full h-1/2 relative overflow-hidden text-center">2</div>
+                    <div className="leading-[0] bg-stone-800 text-white w-full h-1/2 relative overflow-hidden text-center">2</div>
+                  
+                  <div className="absolute h-full w-[100px] top-[0px] perspective-[400px]">
+                    {/* Overlay */}
+                    <div className="absolute overflow-hidden text-center w-full h-1/2 top-[0] leading-[1.5] bg-stone-900 text-white animate-flip-top overlay-bottom">2</div>
+                    <div className="border-t-2 border-black absolute bottom-[0] leading-[0] overflow-hidden text-center w-full h-1/2 bg-stone-800 text-white animation-flip-bottom overlay-top">2</div>
+                  </div>
                 </div>
-                <span className="relative">01</span>
-                <div className="absolute inset-0 flex items-center">
-                  <div className="h-px w-full bg-black/10"></div>
                 </div>
-                </div>
-              
-              <div className="relative bg-[url(/border.png)] p-8">
-                <div className="absolute inset-0 grid grid-rows-2">
-                  <div className="bg-linear-to-br from-stone-700/10 to-stone-900/10"></div>
-                  <div className="bg-linear-to-br from-stone-700/10 to-stone-900/10"></div>
-                </div>
-                <span className="relative">10</span>
-                <div className="absolute inset-0 flex items-center">
-                  <div className="h-px w-full bg-black/10"></div>
-                </div>
-                </div>
-      
-              <div className="relative bg-[url(/border.png)] p-8">
-              <div className="absolute inset-0 grid grid-rows-2">
-                  <div className="bg-linear-to-br from-stone-700/10 to-stone-900/10"></div>
-                  <div className="bg-linear-to-br from-stone-700/10 to-stone-900/10"></div>
-                </div>
-                <span className="relative">20</span>
-                <div className="absolute inset-0 flex items-center">
-                  <div className="h-px w-full bg-black/10"></div>
-                </div>
-                </div>
+              </div>
+              </div>
+  
             </div>
             
           </div>
@@ -44,3 +47,4 @@ export default function Home() {
           </main>
   );
 }
+
